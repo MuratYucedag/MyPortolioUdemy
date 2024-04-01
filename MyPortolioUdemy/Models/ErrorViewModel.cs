f24c7fe10c80a6ed1e425d1d5831003751c4af1d
MuratYucedag <yucedagmurat23@gmail.com>
2024-04-01 09:42:19 +0300

    Add project files.

---
+++ MyPortolioUdemy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyPortolioUdemy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
